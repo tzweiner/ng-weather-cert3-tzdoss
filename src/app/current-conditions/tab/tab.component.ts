ef4518c3d1ba196946cabd8f6536819447029454
TZ Dosseva <singthat@gmail.com>
2024-02-12 09:22:45 -0500

    Some changes to templates

---
+++ src/app/current-conditions/tab/tab.component.ts
@@ -0,0 +1,9 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-tab',
+  templateUrl: './tab.component.html',
+  styleUrls: ['./tab.component.css']
+})
+export class TabComponent {
+}
